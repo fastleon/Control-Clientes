ace47dcc84cf5a8b4ebf593a91ba8d1adeabd3bd
SpyasDev <79065468+fastleon@users.noreply.github.com>
2022-11-29 22:09:02 -0500

    Se aplica correcciones, se cambia request del metodo Get y se envian parametros por medio de HttpSession.

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -10,13 +10,72 @@ import javax.servlet.http.*;
 
 @WebServlet("/ServletControlador")
 public class ServletControlador extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Cliente> clientes = new ClienteDaoJDBC().listar();
         System.out.println("clientes = " + clientes);
-        request.setAttribute("clientes", clientes);
-        request.getRequestDispatcher("clientes.jsp").forward(request, response);
-
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("clientes", clientes);
+        sesion.setAttribute("totalClientes", clientes.size());
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
+        //El siguiente codigo hace que el URL mantenga la ultima acccion por lo que al actualiza
+        //si se agrego un item o se elimino, el navegador tratara de volver a hacerlo.
+        //request.getRequestDispatcher("clientes.jsp").forward(request, response);*/
+        response.sendRedirect("clientes.jsp"); //Abre cliente al igual que el anterior pero notifica el URL del navegador.
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Entre en el metodo doGet, antes de insertar");
+        this.accionDefault(request, response);
+    }
+    
+    private double calcularSaldoTotal(List<Cliente> clientes){
+        double saldoTotal = 0;
+        for(Cliente cliente: clientes){
+            saldoTotal += cliente.getSaldo();
+        }
+        return saldoTotal;
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String accion = request.getParameter("accion");
+        System.out.println("Entre en el metodo doPost, antes de insertar");
+        if(accion != null){
+            switch(accion){
+                case "insertar": 
+                    this.insertarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+            }
+        }
+        else{
+            this.accionDefault(request, response);
+        }
+    }
+    
+    private void insertarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //Recuperamos los valores del formulario agregarCliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if (saldoString != null && !"".equals(saldoString)){
+            saldo = Double.parseDouble(saldoString);
+        }
+        
+        //Creamos el objeto de cliente (modelo)
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
+        
+        //Insertamos el nuevo objeto en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
+        System.out.println("Registros modificados = " + registrosModificados);
+        
+        //Redirigimos hacia la accion por default
+        this.accionDefault(request, response);
     }
 }
